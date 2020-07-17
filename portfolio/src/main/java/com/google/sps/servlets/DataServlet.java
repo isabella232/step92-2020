8e3c30c56479d9c2b15200b11fafc44092721972
Andrew <amgenie364@gmail.com>
2020-07-17 23:41:57 +0000

    remove commented out code and improve code format

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -45,110 +45,95 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Iterator;
 
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    int numberOfCommentsToDisplay = 0;
-    List<String> tagsToSearch = new ArrayList<String>();
-    List<String> messageReplies = new ArrayList<String>(); // TODO: Handle replies later.
+  int numberOfCommentsToDisplay = 0;
+  List<String> tagsToSearch = new ArrayList<String>();
+  List<String> messageReplies = new ArrayList<String>(); 
+  // TODO: Handle replies later.
     
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException { 
-      List<BlogMessage> messages = new ArrayList<>();
-      Query query = new Query("blogMessage").addSort("time", SortDirection.ASCENDING);
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      PreparedQuery results = datastore.prepare(query);
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+    List<BlogMessage> messages = new ArrayList<>();
+    Query query = new Query("blogMessage").addSort("time", SortDirection.ASCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
 
-      UserService userService = UserServiceFactory.getUserService();
+    UserService userService = UserServiceFactory.getUserService();
 
-      for (Entity entity : results.asIterable()) {
-        long messageId = entity.getKey().getId();
-        long timestamp = (long) entity.getProperty("time");
-        String tag = (String) entity.getProperty("tag");
-        String comment = (String) entity.getProperty("text");
-        String nickname = (String) entity.getProperty("nickname");
-        String email = (String) userService.getCurrentUser().getEmail();
-        //String image = (String) entity.getProperty("imgUrl");
-        ArrayList<String> message_Replies = (ArrayList) entity.getProperty("replies");
-        BlogMessage message = new BlogMessage(messageId, tag, comment, nickname, email, message_Replies, timestamp);
-        messages.add(message);
-      }
+    for (Entity entity : results.asIterable()) {
+      long messageId = entity.getKey().getId();
+      long timestamp = (long) entity.getProperty("time");
+      String tag = (String) entity.getProperty("tag");
+      String comment = (String) entity.getProperty("text");
+      String nickname = (String) entity.getProperty("nickname");
+      String email = (String) userService.getCurrentUser().getEmail();
+      ArrayList<String> messageReplies = (ArrayList) entity.getProperty("replies");
+      BlogMessage message = new BlogMessage(messageId, tag, comment, nickname, email, messageReplies, timestamp);
+      messages.add(message);
+    }
       
-      // Create BlogHashMap Object and put BlogMessages in the map.
-      BlogHashMap blogMap = new BlogHashMap();
-      blogMap.putInMap(messages);
+    // Create BlogHashMap Object and put BlogMessages in the map.
+    BlogHashMap blogMap = new BlogHashMap();
+    blogMap.putInMap(messages);
 
-       // Load messages from BlogHashMap and respond with gson.
-      LinkedList<BlogMessage> loadedBlogMessages = blogMap.getMessages(tagsToSearch, numberOfCommentsToDisplay);
+    // Load messages from BlogHashMap and respond with gson.
+    LinkedList<BlogMessage> loadedBlogMessages = blogMap.getMessages(tagsToSearch, numberOfCommentsToDisplay);
     
-      Gson gson = new Gson();
-      response.setContentType("application/json;");
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
       
-      response.getWriter().println(gson.toJson(limitedBlogMessages));
+    response.getWriter().println(gson.toJson(loadedBlogMessages));
+    return;
+    }
+    
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get Post parameters.
+    String message = request.getParameter("text-input");
+    String nickname = request.getParameter("sender");
+    String postTag = request.getParameter("tags");
+
+    if (postTag.isEmpty() || postTag == null) {
+      postTag = InternalTags.defaultTag();
+    }
+      
+    //TODO: Handle image file sent with FormData.
+      
+    // Only put BlogMessages with a message in datastore.
+    if (message.isEmpty() || message == null) {
       return;
     }
+
+    long timestamp = System.currentTimeMillis();
+    Entity blogMessageEntity = new Entity("blogMessage");
+    blogMessageEntity.setProperty("nickname", nickname);
+    blogMessageEntity.setProperty("text", message);
+    blogMessageEntity.setProperty("time", timestamp);
+    blogMessageEntity.setProperty("tag", postTag);
+    blogMessageEntity.setProperty("replies", messageReplies);
     
-   @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      // Get Post parameters.
-      String message = request.getParameter("text-input");
-      String nickname = request.getParameter("sender");
-      String loadFactor = request.getParameter("load_factor");
-      String postTag = getTagParameter(request, "tags", InternalTags.defaultTag());
-      
-      //TODO: Handle image file sent with FormData.
-      //String imageUrl = getUploadedFileUrl(request, imagePath);
-      
-      // Only put BlogMessages with a message and tag in datastore.
-      if (message.equals("") || message.equals(null) || postTag == null) {
-        return;
-      }
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(blogMessageEntity);
 
-      long timestamp = System.currentTimeMillis();
-      Entity blogMessageEntity = new Entity("blogMessage");
-      blogMessageEntity.setProperty("nickname", nickname);
-      blogMessageEntity.setProperty("text", message);
-      // blogMessageEntity.setProperty("imgUrl", imageUrl);
-      blogMessageEntity.setProperty("time", timestamp);
-      blogMessageEntity.setProperty("tag", postTag);
-      blogMessageEntity.setProperty("replies", messageReplies);
+    // To get the recently posted message, add its tag to the |tagsToSearch| list.
+    // If the list has tags already, clear them before adding the tag.
+    // Our goal is to get the recently posted message.
+    tagsToSearch.clear();
+    tagsToSearch.add(postTag);
     
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      datastore.put(blogMessageEntity);
-
-      // To get the recently posted message, add its tag to the |tagsToSearch| list.
-      // If the list has tags already, clear them before adding the tag.
-      // Our goal is to get the recently posted message.
-      if (!tagsToSearch.isEmpty()) {
-        tagsToSearch.clear();
-      }
-      tagsToSearch.add(postTag);
-
-      // Everytime a post comes through, the javascript function that handles the Post assigns
-      // a load_factor value of 1.
-      // We assign that to the |numberOfCommentsToDisplay|. 
-      if (loadFactor != null) {
-        // we don't catch since this is always 1.
-        numberOfCommentsToDisplay = Integer.parseInt(loadFactor); 
-      }
-      
-      // Now we call |doGet| to load and respond with the message.
-      // |doGet| passes the |tagsToSearch| and |numberOfCommentsToDisplay| to the 
-      // BlogHashMap's getMessages method, which responds with the recent post.
-      doGet(request, response);
-    }
-
-    // Returns the requested parameter, or the default value if the parameter is null.
-    private String getTagParameter(HttpServletRequest request, String name, String defaultValue) {
-      String value = request.getParameter(name);
-      if (value == null) {
-        return defaultValue;
-      }
-      return value;
+    // We want to load only the recent post.
+    numberOfCommentsToDisplay = 1; 
+    
+    // Now we call |doGet| to load and respond with the message.
+    // |doGet| passes the |tagsToSearch| and |numberOfCommentsToDisplay| to the 
+    // BlogHashMap's getMessages method, which responds with the recent post.
+    doGet(request, response);
     }
 
     // Returns a URL that points to the uploaded file, or null if the user didn't upload a file.
