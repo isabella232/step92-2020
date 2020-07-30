3900ffcf852d5225afb9444f9bbd0dbebfdb09a4
tayyaba17 <tayyabaxali@gmail.com>
2020-07-30 18:11:59 +0000

    Add all tests.

--- portfolio/src/test/java/com/google/sps/CuratedPostsTest.java
+++ portfolio/src/test/java/com/google/sps/CuratedPostsTest.java
@@ -6,13 +6,7 @@ import static org.junit.Assert.assertEquals;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.images.ServingUrlOptions;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import org.junit.After;
@@ -20,8 +14,6 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
-
-
 
 public class CuratedPostsTest {
 
@@ -40,11 +32,24 @@ public class CuratedPostsTest {
 
   private void createDatastoreEntities() {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Entity UserEntity = new Entity("UserLogin");
-    UserEntity.setProperty("id", "246810");
-    UserEntity.setProperty("nickname", "hasTags");
-    UserEntity.setProperty("email", "hasTags@testing.com");
-    datastore.put(UserEntity);
+
+    Entity noTagEntity = new Entity("UserLogin");
+    noTagEntity.setProperty("id", "357911");
+    noTagEntity.setProperty("nickname", "noTags");
+    noTagEntity.setProperty("email", "noTags@testing.com");
+    datastore.put(noTagEntity);
+
+    Entity hasTagEntity = new Entity("UserLogin");
+    hasTagEntity.setProperty("id", "246810");
+    hasTagEntity.setProperty("nickname", "hasTags");
+    hasTagEntity.setProperty("email", "hasTags@testing.com");
+    datastore.put(hasTagEntity);
+
+    Entity hasMultipleTagsEntity = new Entity("UserLogin");
+    hasMultipleTagsEntity.setProperty("id", "123456");
+    hasMultipleTagsEntity.setProperty("nickname", "hasMultipleTags");
+    hasMultipleTagsEntity.setProperty("email", "hasMultipleTags@testing.com");
+    datastore.put(hasMultipleTagsEntity);
 
     Entity blogMessageEntity = new Entity("blogMessage");
     blogMessageEntity.setProperty("nickname", "test1");
@@ -54,11 +59,30 @@ public class CuratedPostsTest {
     blogMessageEntity.setProperty("parentID", 0);
     datastore.put(blogMessageEntity);
 
-    Entity followedTag = new Entity("followedTag");
-    followedTag.setProperty("tag", "#business");
-    followedTag.setProperty("email", "hasTags@testing.com");
-    datastore.put(followedTag);
+    Entity hasTagFollowedTags = new Entity("followedTag");
+    hasTagFollowedTags.setProperty("tag", "#business");
+    hasTagFollowedTags.setProperty("email", "hasTags@testing.com");
+    datastore.put(hasTagFollowedTags);
+
+    List<String> hasMultipleTagsFollowed = new ArrayList<String>();
+    hasMultipleTagsFollowed.add("#business");
+    hasMultipleTagsFollowed.add("#education");
+    hasMultipleTagsFollowed.add("#music");
     
+    for (int i=0; i<hasMultipleTagsFollowed.size(); i++) {
+      Entity hasMultipleTagsFollowedTags = new Entity("followedTag");
+      hasMultipleTagsFollowedTags.setProperty("tag", hasMultipleTagsFollowed.get(i));
+      hasMultipleTagsFollowedTags.setProperty("email", "hasMultipleTags@testing.com");
+      datastore.put(hasMultipleTagsFollowedTags);
+    }
+
+  }
+
+  @Test
+  public void testFollowsNoTags() {
+    createDatastoreEntities();
+    List<String> expectedFollowedTags = new ArrayList<String>();
+    assertEquals(expectedFollowedTags, LoadFollowedTags.getFollowedTags("noTags@testing.com"));
   }
 
   @Test
@@ -67,9 +91,32 @@ public class CuratedPostsTest {
     List<String> expectedFollowedTags = new ArrayList<String>();
     expectedFollowedTags.add("#business");
     assertEquals(expectedFollowedTags, LoadFollowedTags.getFollowedTags("hasTags@testing.com"));
+    assertEquals(1, LoadFollowedTags.getFollowedTags("hasTags@testing.com").size());
+  }
+
+    @Test
+  public void testFollowsMultipleTag() {
+    createDatastoreEntities();
+    List<String> expectedFollowedTags = new ArrayList<String>();
+    expectedFollowedTags.add("#business");
+    expectedFollowedTags.add("#education");
+    expectedFollowedTags.add("#music");
+    assertEquals(expectedFollowedTags, LoadFollowedTags.getFollowedTags("hasMultipleTags@testing.com"));
+    assertEquals(3, LoadFollowedTags.getFollowedTags("hasMultipleTags@testing.com").size());
   }
 
   @Test
-  public void testFollowsNoTags() {
+  public void testFollowsTagsFalse() {
+    createDatastoreEntities();
+    Boolean expectedFollowedTags = false;
+    assertEquals(expectedFollowedTags, LoadFollowedTags.hasFollowedTags("noTags@testing.com"));
+  }
+
+  @Test
+  public void testFollowsTagsTrue() {
+    createDatastoreEntities();
+    Boolean expectedFollowedTags = true;
+    assertEquals(expectedFollowedTags, LoadFollowedTags.hasFollowedTags("hasTags@testing.com"));
+    assertEquals(expectedFollowedTags, LoadFollowedTags.hasFollowedTags("hasMultipleTags@testing.com"));
   }
 }
