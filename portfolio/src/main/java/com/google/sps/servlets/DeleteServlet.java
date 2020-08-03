5ea8323bf2125c2020cf696d1742dc3c51c33093
Andrewmob2703 <66045789+Andrewmob2703@users.noreply.github.com>
2020-08-03 15:59:21 -0500

    Update DeleteServlet to
    delete any entity from datastore once its kind is received.
    Also sends an appropriate message to the client.

--- portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -23,25 +23,35 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
+import java.util.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-/** Servlet responsible for deleting tasks. */
+
+/** Servlet responsible for deleting entities from datastore. */
 @WebServlet("/delete-data")
 public class DeleteServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long messageId = Long.parseLong(request.getParameter(BlogConstants.ID_PARAMETER));
+    long entityID = Long.parseLong(request.getParameter(BlogConstants.ENTITY_ID_PARAMETER));
+    String entityKind = request.getParameter(BlogConstants.ENTITY_KIND_PARAMETER);
 
-    Key postEntityKey = KeyFactory.createKey(BlogConstants.BLOG_ENTITY_KIND, messageId);
+    Key EntityKey = KeyFactory.createKey(entityKind, entityID);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.delete(postEntityKey);
+    datastore.delete(EntityKey);
 
     response.setContentType("text/html");
-    response.getWriter().println("Success! Post Deleted.");
+    if (entityKind.equals(BlogConstants.BLOG_ENTITY_KIND)) {
+      response.getWriter().println("Success! Post Deleted.");
+    }
+    if (entityKind.equals(BlogConstants.TAG_QUERY)) {
+      response.getWriter().println("Success! You unfollow this tag.");
+    }
   }
+
 }
+/* End Of File */
