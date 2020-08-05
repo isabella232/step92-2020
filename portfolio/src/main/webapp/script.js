f5e2d8d7f6b7cf767caa170fc53ff72ae679eeab
Andrewmob2703 <66045789+Andrewmob2703@users.noreply.github.com>
2020-08-04 19:37:25 -0500

    Implement functions to handle replies
    under their respective posts

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -53,21 +53,24 @@ document.getElementById('post-form').addEventListener('submit', function(postSub
 // Reply event listener to submit replies with Fetch on form submit.
 document.getElementById('reply-form').addEventListener('submit', function(replySubmitEvent) {
   replySubmitEvent.preventDefault();
-
+ 
   const formData = new FormData(this);
   const replyFormParams = new URLSearchParams();
   
   for (const pair of formData) {
     replyFormParams.append(pair[0], pair[1]);
   }
-
-  fetch('/data', {method: 'POST', body: replyFormParams}).catch (function(error) {
+ 
+  fetch('/data', {method: 'POST', body: replyFormParams})
+        .then(response => response.json()).then((replies) => {
+    replies.forEach((reply) => {
+      let postRepliesContainer = document.getElementById(`repliesli${reply.parentID}`);
+      postRepliesContainer.appendChild(createRepliesElements(reply));
+    });
+    resetForm('reply-form');
+  }).catch (function(error) {
       console.log(error);
-      return;
   });
-  // TODO: display replies under their respective posts.
-
-  resetForm('reply-form');
 });
 
 function openPage(pageName, elmnt, color) {
@@ -98,23 +101,31 @@ function defaultPage() {
 
 // Makes a Get request and loads Posts to the Blog page on body load
 // Also loads 5 display-only posts to be displayed on the home page.
-async function loadPosts(){
+async function loadPosts() {
   const HOME_LOAD_AMOUNT = 5;
   fetch('/data').then(response => response.json()).then((msgs) => {
     const statsListElement = document.getElementById('posts-list');
-    const homeListElement = document.getElementById('home-comments-container');
+    const homeListElement = document.getElementById('home-comments-tagselectcontainer');
     var i = 1;
     msgs.forEach((msg) => {
-      statsListElement.appendChild(createListElement(msg));
+      statsListElement.appendChild(createPostsElements(msg));
+      let msgReplies = msg.messageReplies;
+      let repliesContainer = document.querySelector(`#repliesli${CSS.escape(msg.id)}`);
+      if (!isEmpty(msgReplies)) {
+        msgReplies.forEach((reply) => {
+          repliesContainer.appendChild(createRepliesElements(reply));
+        });
+      }
+ 
       if (i > HOME_LOAD_AMOUNT) {
         return true;
       }
-      homeListElement.appendChild(createListElementHome(msg));
+      homeListElement.appendChild(createPostsElementsHome(msg));
       i++;
-    })
+    });
   }).catch (function(error) {
     console.log(error);
-  })
+  });
 }
 
 function getCommentsTag(tag) {
@@ -222,6 +233,49 @@ function createListElementHome(msg) {
   return postElement;
 }
 
+// Returns an <li> element containing reply details.
+function createRepliesElements(reply) {
+  const replyElement = document.createElement('li');
+  replyElement.id = `reply${reply.id}`;
+  replyElement.className = 'reply';
+  
+  const replyContainer = document.createElement('span');
+
+  const messageElement = document.createElement('span');
+  messageElement.className = 'replyText';
+  messageElement.innerText = reply.message;
+  
+  const userElement = document.createElement('span');
+  userElement.className = 'postSender';
+  if (reply.nickname === undefined || reply.nickname === null) {
+    userElement.innerHTML = "<i>_Anonymous</i>";
+  } else {
+    userElement.innerHTML = "<i>_" + reply.nickname + "</i>";
+  }
+ 
+  const timeElement = document.createElement('span');
+  timeElement.className = 'postDate';
+  var date = new Date(reply.timestamp);
+  timeElement.innerText = date.toLocaleString().slice(0, 24);
+ 
+  const deleteReplyBtn = document.createElement('button');
+  deleteReplyBtn.className = 'replyDelBtn';
+  deleteReplyBtn.innerText = 'Delete Reply';
+  deleteReplyBtn.addEventListener('click', () => {
+    const REPLY_ENTITY_KIND = 'blogMessage';
+    deleteElement(reply, REPLY_ENTITY_KIND, replyElement);
+  });
+  
+  replyContainer.appendChild(userElement);
+  replyContainer.appendChild(messageElement);
+  replyContainer.appendChild(timeElement);
+  replyContainer.appendChild(deleteReplyBtn);
+
+  replyElement.appendChild(replyContainer);
+ 
+  return replyElement;
+}
+
 /** Creates an <img> element containing text. */
 function createImgElement(text) {
   const imgElement = document.createElement('img');
@@ -268,9 +322,9 @@ async function deleteElement(entityObj, entityKind, elemContainer) {
 // Removes the reply form if button is clicked again, and so on...
 function handleReplies(msgID, msgSender, tag, postEleID) { 
   // The form appears when a reply button is clicked an even number of times, so
-  // If a users clicks a different reply button, set counter to zero for that button
+  // If a different reply button is clicked, form is reset; counter is set to 0.
   if (clickedDifferentReplyButton(msgID)) {
-    replyClickCounter = 0;
+    resetForm('reply-form');
   }
   
   // Remove the form when a button is clicked an odd number of times in a row.
@@ -293,13 +347,13 @@ function handleReplies(msgID, msgSender, tag, postEleID) {
     if (replyArea.value.length >= 1) {
       document.getElementById('reply-submit').style.display = 'block';
       document.getElementById('reply-submit').innerHTML = '<i>Reply Post</i>';
-      replyArea.style.backgroundColor = 'grey';
+      replyArea.style.backgroundColor = 'LightGray';
     } else {
       document.getElementById('reply-submit').style.display = 'none';
       replyArea.style.backgroundColor = 'white';
     }
   });
-
+ 
   // On submit, a reply's parentID and tag is set to those of its post.
   document.getElementById('reply-parentID').value = msgID;
   document.getElementById('reply-tag').value = tag;
